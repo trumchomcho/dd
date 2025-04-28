2db0a6e6dffab5cfacbc7c8e995eb07090bcf322
DinhPhu <dolanxknt@gmail.com>
2025-04-28 19:51:09 +0700

    khanwareMinimal.js

--- khanwareMinimal.js
+++ khanwareMinimal.js
@@ -80,7 +80,7 @@ function sendToast(text, duration = 5000, gravity = 'bottom') {
 
 async function showSplashScreen() {
     splashScreen.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;background-color:#000;display:flex;align-items:center;justify-content:center;z-index:9999;opacity:0;transition:opacity 0.5s ease;user-select:none;color:white;font-family:MuseoSans,sans-serif;font-size:30px;text-align:center;";
-    splashScreen.innerHTML = '<span style="color:white;">KHANWARE</span><span style="color:#72ff72;">.SPACE</span>';
+    splashScreen.innerHTML = '<span style="color:white;">khan hack</span><span style="color:#72ff72;">.DPHUDPHU</span>';
     document.body.appendChild(splashScreen);
     setTimeout(() => splashScreen.style.opacity = '1', 10);
 };
