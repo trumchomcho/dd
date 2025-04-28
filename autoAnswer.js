fa8017bfbc8efcb7d1c22f5e3ef1f1cbeb9a5f6f
DinhPhu <dolanxknt@gmail.com>
2025-04-28 12:51:32 +0700

    Update autoAnswer.js

--- autoAnswer.js
+++ autoAnswer.js
@@ -1,9 +1,9 @@
 const baseSelectors = [
-    `[data-testid="choice-icon__library-choice-icon"]`,
-    `[data-testid="exercise-check-answer"]`, 
-    `[data-testid="exercise-next-question"]`, 
-    `._1udzurba`,
-    `._awve9b`
+    `[data-testid="choice-icon__library-choice-icon"]`,    // Chọn icon trong thư viện
+    `[data-testid="exercise-check-answer"]`,               // Nút kiểm tra đáp án
+    `[data-testid="exercise-next-question"]`,              // Nút qua câu hỏi tiếp theo
+    `._1udzurba`,                                           // Một phần tử có class đặc biệt
+    `._awve9b`                                              // Một phần tử khác
 ];
 
 khanwareDominates = true;
@@ -19,8 +19,8 @@ khanwareDominates = true;
 
             for (const q of selectorsToCheck) {
                 findAndClickBySelector(q);
-                if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                    sendToast("🎉 Exercício concluído!", 3000);
+                if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Hiển thị tóm tắt") {
+                    sendToast("🎉 Bài tập đã hoàn thành!", 3000);
                     playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                 }
             }
